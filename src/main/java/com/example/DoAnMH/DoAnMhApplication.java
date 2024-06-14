0f22961ee6413f4ac94635e0cf3a5a50641c1df2
NTT203 <anhtienpro203@gmail.com>
2024-06-14 11:43:04 +0700

    first commit

---
+++ src/main/java/com/example/DoAnMH/DoAnMhApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoAnMH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnMhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnMhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoAnMH/DoAnMhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoAnMH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnMhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
